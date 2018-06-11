bfa5a3a57305dd32d6fc70b704450029735fc858
acabezaslopez <acabezaslopez@victoria11.danielcastelao.org>
2018-06-11 09:55:20 +0200

    exemplo stream

--- src/exemplostream/UsosStreams.java
+++ src/exemplostream/UsosStreams.java
@@ -1,7 +1,6 @@
 package exemplostream;
 
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 import java.util.stream.Collectors;
 import javax.swing.JOptionPane;
@@ -11,10 +10,10 @@ import javax.swing.JOptionPane;
  * @author acabezaslopez
  */
 public class UsosStreams {
-    
+
     List<Alumno> listaAlumnos = new ArrayList();
     List<Alumno> suspensos = new ArrayList();
-    
+
     public void crearLista() {
         listaAlumnos.add(new Alumno("111", "Ana", "dam1", 4));
         listaAlumnos.add(new Alumno("222", "Ana", "dam1", 8));
@@ -23,7 +22,7 @@ public class UsosStreams {
         listaAlumnos.add(new Alumno("555", "Alex", "dam1", 3));
         listaAlumnos.add(new Alumno("666", "Noa", "adm", 7));
     }
-    
+
     public void amosar() {
         /* try {
 
@@ -40,7 +39,7 @@ public class UsosStreams {
         listaAlumnos.stream().forEach((a) -> System.out.println(a));
         // **** UTILIZANDO REFERENCIA A METODOS */
         listaAlumnos.stream().filter(a -> true).forEach(System.out::println);
-        
+
     }
 
     /* public void pedirdatos(){
@@ -57,23 +56,40 @@ public class UsosStreams {
         //listaAlumnos.stream().filter(a -> a.getNome().charAt(0) == 'A').forEach(a -> System.out.println(a));
         listaAlumnos.stream().filter(al -> al.getNome().startsWith("B")).forEach(al -> System.out.println(al));
     }
-    
+
     public void aprobados() {
         /* ***AMOSAR OS APROBADOS *** */
         listaAlumnos.stream().filter(al -> al.getNota() >= 5).forEach(al -> System.out.println(al));
     }
-    
+
     public void listarSuspensos() {
         suspensos = listaAlumnos.stream().filter((al) -> al.getNota() < 5).collect(Collectors.toList());
-        
+
     }
-    
+
     public void amosarSuspensos() {
         suspensos.stream().forEach(al -> System.out.println(al));
     }
-    
-    public void buscarAlumo() {
+
+    public void buscarAlumno() {
         String nif = JOptionPane.showInputDialog("Introduce dni :");
         listaAlumnos.stream().filter(al -> al.getDni().equalsIgnoreCase(nif)).forEach(System.out::println);
     }
+   
+      public void mayorNota(){
+            
+           crearLista();
+         System.out.println(listaAlumnos.stream().max((al1,al2)->al1.getNota()-al2.getNota()).get());
+      }
+      public void ordenar(){
+          crearLista();
+          listaAlumnos.stream().sorted((al1,al2)-> al1.getNome().compareToIgnoreCase(al2.getNome())).forEach(System.out::println);
+      }
+      public void subirNota(){
+          crearLista();
+          
+          listaAlumnos.stream().map((al)-> al.getNota()+2).forEach(System.out::println);
+          listaAlumnos.stream().filter(al -> al.getNota()>5).map(al -> al.getNota()+2).forEach(System.out::println);
+      }
+
 }
